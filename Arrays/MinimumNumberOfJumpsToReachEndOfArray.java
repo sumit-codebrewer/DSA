b37d0ac877be525f1a972fafdef8935a6713020a
Sumit Chutani <sumitengineer23@gmail.com>
2022-01-03 20:01:29 +0530

    Added a new array program solved using DP approach

---
+++ Arrays/MinimumNumberOfJumpsToReachEndOfArray.java
@@ -0,0 +1,36 @@
+package loveBabbarCheatsheet;
+
+public class MinimumNumberOfJumpsToReachEndOfArray {
+
+	public static void main(String[] args) {
+		int []arr={1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9} ;
+      	int result=minJumps(arr);
+      	System.out.println("Minimum number of jumps required:"+result);
+
+	}
+	 public static int minJumps(int[] arr){
+	        int n=arr.length;
+	        if(n<=1)
+	            return 0;
+	        if(arr[0]==0)
+	            return -1;
+	        int maxReach=arr[0];
+	        int step=arr[0];
+	        int jump=1;
+	        for(int i=1;i<n;i++){
+	            if(i==n-1){
+	                return jump;
+	            }
+	            maxReach=Math.max(maxReach,i+arr[i]);
+	            step--;
+	            if(step==0){
+	                jump++;
+	                if(i>=maxReach){
+	                    return -1;
+	                }
+	                step=maxReach-i;
+	            }
+	        }
+	        return -1;
+	    }
+}
